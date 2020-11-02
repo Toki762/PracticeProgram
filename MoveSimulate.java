5171eb8a560a26f75c0efaa8b3d0609f6ca1c61d
Toki1729 <56390698+Toki762@users.noreply.github.com>
2020-11-02 14:38:08 +0900

    Update and rename 2DMoveSimulate.java to MoveSimulate.java

--- MoveSimulate.java
+++ MoveSimulate.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class 2DMoveSimulate {
+public class MoveSimulate {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
